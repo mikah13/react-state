b9be32e5d098405f11e1fe4dc17fea6476581106
mikah13 <anhminhhoang13@gmail.com>
2024-01-08 05:39:28 -0800

    added: rtk store

---
+++ src/states/rtk.ts
@@ -0,0 +1,24 @@
+import { defaultStudent, registerCourse } from '@/lib/student';
+import { configureStore } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export const studentSlice = createSlice({
+  name: 'student',
+  initialState: defaultStudent,
+  reducers: {
+    reset: () => {
+      return defaultStudent;
+    },
+    addCourse: (state) => {
+      return registerCourse(state);
+    },
+  },
+});
+export const { reset, addCourse } = studentSlice.actions;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
